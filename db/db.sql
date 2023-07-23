df4c3a6017a4f3040a0f6b7ce914e2ee24e3d98f
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-22 20:01:16 -0500

    feat: :card_file_box: Insert data
    Se insertó data a todas las tablas de la base de datos, probando de que las llaves foraneas sirvan completamente

--- db/db.sql
+++ db/db.sql
@@ -64,3 +64,83 @@ DROP TABLE experiencia;
 DROP TABLE daño;
 DROP TABLE usuario;
 DROP TABLE sismo;
+
+
+---------Inserte Info----
+
+INSERT INTO daño (idSismo, tipoDaño, descripcion)
+VALUES
+  (1, 'Estructural', 'Daño en edificio A'),
+  (2, 'Vial', 'Daño en carretera principal'),
+  (3, 'Inundación', 'Daño por inundación en área C');
+
+INSERT INTO localizacion (latitud, longitud, ciudad)
+VALUES
+  (12.345678, -98.765432, 'Ciudad A'),
+  (23.456789, -87.654321, 'Ciudad B'),
+  (34.567890, -76.543210, 'Ciudad C');
+
+INSERT INTO usuario (nombre, apellido, correo, telefono, idLocalizacion)
+VALUES
+  ('Juan', 'Pérez', 'juan@example.com', 1234567890, 1),
+  ('María', 'Gómez', 'maria@example.com', 9876543210, 2),
+  ('Pedro', 'López', 'pedro@example.com', 5555555555, 3);
+
+INSERT INTO experiencia (idUsuario, idSismo, fecha, tex_comentario, idDaño)
+VALUES
+  (1, 1, '2023-07-01', 'Excelente experiencia', 1),
+  (2, 2, '2023-07-05', 'Muy impactante', 2),
+  (3, 3, '2023-07-10', 'Ayuda humanitaria necesaria', 3);
+
+
+
+INSERT INTO localizacion (latitud, longitud, ciudad)
+VALUES
+  (45.678912, -56.789012, 'Ciudad D'),
+  (56.789012, -67.890123, 'Ciudad E'),
+  (67.890123, -78.901234, 'Ciudad F');
+
+INSERT INTO usuario (nombre, apellido, correo, telefono, idLocalizacion)
+VALUES
+  ('Ana', 'Sánchez', 'ana@example.com', 1111111111, 1),
+  ('Carlos', 'González', 'carlos@example.com', 2222222222, 2),
+  ('Sofía', 'Martínez', 'sofia@example.com', 3333333333, 3);
+
+INSERT INTO daño (idSismo, tipoDaño, descripcion)
+VALUES
+  (4, 'Incendio', 'Incendio en almacén'),
+  (5, 'Deslizamiento', 'Deslizamiento de tierra en zona montañosa'),
+  (6, 'Inundación', 'Inundación por lluvias intensas');
+
+INSERT INTO experiencia (idUsuario, idSismo, fecha, tex_comentario, idDaño)
+VALUES
+  (4, 4, '2023-07-15', 'Respuesta rápida de los equipos de emergencia', 4),
+  (5, 5, '2023-07-20', 'Daño considerable en la infraestructura', 5),
+  (6, 6, '2023-07-25', 'Comunidad afectada necesita apoyo', 6);
+
+
+-----Dato de localizacion enviada por archivo localizacion
+
+INSERT INTO localizacion (latitud, longitud, ciudad)
+VALUES
+  (78.901234, -89.012345, 'Ciudad G'),
+  (89.012345, -90.123456, 'Ciudad H'),
+  (90.123456, -91.234567, 'Ciudad I');
+
+INSERT INTO usuario (nombre, apellido, correo, telefono, idLocalizacion)
+VALUES
+  ('Luis', 'Ramírez', 'luis@example.com', 4444444444, 4),
+  ('Marta', 'López', 'marta@example.com', 5555555555, 5),
+  ('Jorge', 'Hernández', 'jorge@example.com', 6666666666, 6);
+
+INSERT INTO daño (idSismo, tipoDaño, descripcion)
+VALUES
+  (7, 'Colapso', 'Colapso parcial de puente'),
+  (8, 'Erosión', 'Erosión costera en playa turística'),
+  (9, 'Avalancha', 'Avalancha en zona de montaña');
+
+INSERT INTO experiencia (idUsuario, idSismo, fecha, tex_comentario, idDaño)
+VALUES
+  (7, 7, '2023-07-30', 'Evacuación de la población necesaria', 7),
+  (8, 8, '2023-08-05', 'Importante daño en infraestructura turística', 8),
+  (9, 9, '2023-08-10', 'Equipos de emergencia atendiendo la situación', 9);
