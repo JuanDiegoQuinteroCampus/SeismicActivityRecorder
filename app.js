65192a5795b4266403b31773a87888d3803f37f4
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-23 01:34:55 -0500

    feat: :sparkles: CRUD tabla usuario
    Se realizó un crud de la tabla usuario donde se obtenian, enviaba, actualizaban y eliminaban datos de la tabla

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import storageSismo from './routers/sismo.js';
 import storageLocalizacion from './routers/localizacion.js';
 import storageDaño from './routers/daño.js';
+import storageUsuario from './routers/usuario.js';
 
 
 dotenv.config();
@@ -13,6 +14,7 @@ appExpress.use(express.json());
 appExpress.use("/sismo", storageSismo);
 appExpress.use("/localizacion", storageLocalizacion)
 appExpress.use("/dano", storageDaño)
+appExpress.use("/user", storageUsuario)
 
 
 const config = JSON.parse(process.env.MY_CONFIG);
---
+++ routers/usuario.js
@@ -0,0 +1,83 @@
+import mysql from "mysql2";
+import { Router } from "express";
+
+const storageUsuario = Router();
+let con = undefined;
+
+storageUsuario.use((req, res, next) =>{
+    let myConfig = JSON.parse(process.env.MY_CONNECT)
+    con = mysql.createPool(myConfig);
+    next();
+});
+
+storageUsuario.get('/info', (req, res)=>{
+    con.query(
+        `SELECT * FROM usuario`,
+        (err,data,fil) => {
+            res.send(JSON.stringify(data));
+        }
+    )
+});
+
+storageUsuario.post('/sent', (req, res)=>{
+    
+    const {
+        nombre, apellido, correo, telefono, idLocalizacion
+    } = req.body;
+    
+    const sql =`
+    INSERT INTO usuario (nombre, apellido, correo, telefono, idLocalizacion)
+    VALUES (?, ?, ?,?,?)
+    `;
+
+    const values = [
+        nombre, apellido, correo, telefono, idLocalizacion
+    ];
+    
+    con.query(sql, values, (err, result) => {
+        if (err) {
+            console.error("Error al insertar los datos del usuario:", err);
+            return res.status(500).json({ error: "Error interno del servidor."})
+        }
+        res.status(201).json({ mensaje: "datos del usuario insertados con éxito." });
+    });
+});
+
+storageUsuario.put('/update/:idUsuario', (req, res) => {
+    const idUsuario = req.params.idUsuario;
+    const newData = req.body; 
+  
+    con.query(
+      'UPDATE usuario SET ? WHERE idUsuario = ?',
+      [newData, idUsuario],
+      (err, result) => {
+        if (err) {
+          console.error('Error al actualizar el registro:', err);
+          res.status(500).send('Error al actualizar el registro');
+        } else {
+          console.log('Registro actualizado:', result.affectedRows, 'filas afectadas');
+          res.status(200).send('Registro actualizado exitosamente');
+        }
+      }
+    );
+  });
+
+
+  storageUsuario.delete('/del/:idUsuario', (req, res)=>{
+    const idUsuario = req.params.idUsuario;
+    con.query(
+        `DELETE FROM usuario WHERE idUsuario = ?`,
+        idUsuario,
+
+        (err, result) => {
+            if (err) {
+                console.error('Error al eliminar los datos de usuarios', err.message);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    )
+});
+
+export default storageUsuario;
