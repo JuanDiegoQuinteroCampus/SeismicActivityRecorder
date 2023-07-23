c5fb16ff89a343f970b19d84d21bb4c42cc67a15
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-07-23 00:11:33 -0500

    feat: :sparkles: CRUD loaclizacion
    Se hizo el crud de localizacion con sus respectivos metodos de consulta, get, post, put y delete

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 /* import sismo from './api/sismoApi.js'; */
 import storageSismo from './routers/sismo.js';
+import storageLocalizacion from './routers/localizacion.js';
 
 dotenv.config();
 const appExpress = express();
 
 appExpress.use(express.json());
-appExpress.use("/sismo", storageSismo)
+appExpress.use("/sismo", storageSismo);
+appExpress.use("/localizacion", storageLocalizacion)
 
 
 const config = JSON.parse(process.env.MY_CONFIG);
---
+++ routers/localizacion.js
@@ -0,0 +1,87 @@
+import mysql from "mysql2";
+import { Router } from "express";
+
+const storageLocalizacion = Router();
+let con = undefined;
+
+storageLocalizacion.use((req, res, next) =>{
+    let myConfig = JSON.parse(process.env.MY_CONNECT)
+    con = mysql.createPool(myConfig);
+    next();
+});
+
+storageLocalizacion.get('/lugar', (req, res)=>{
+    con.query(
+        `SELECT * FROM localizacion`,
+        (err,data,fil) => {
+            res.send(JSON.stringify(data));
+        }
+    )
+});
+
+storageLocalizacion.post('/lugar/sent', (req, res)=>{
+    
+    const {
+        latitud,
+        longitud,
+        ciudad
+    } = req.body;
+    
+    const sql =`
+    INSERT INTO localizacion (latitud, longitud, ciudad)
+    VALUES (?, ?, ?)
+    `;
+
+    const values = [
+        latitud,
+        longitud,
+        ciudad,
+    ];
+    
+    con.query(sql, values, (err, result) => {
+        if (err) {
+            console.error("Error al insertar la la localidad:", err);
+            return res.status(500).json({ error: "Error interno del servidor."})
+        }
+        res.status(201).json({ mensaje: "Localidad insertada con éxito." });
+    });
+});
+
+storageLocalizacion.put('/lugar/update/:idLocalizacion', (req, res) => {
+    const idLocalizacion = req.params.idLocalizacion;
+    const newData = req.body; 
+  
+    con.query(
+      'UPDATE localizacion SET ? WHERE idLocalizacion = ?',
+      [newData, idLocalizacion],
+      (err, result) => {
+        if (err) {
+          console.error('Error al actualizar el registro:', err);
+          res.status(500).send('Error al actualizar el registro');
+        } else {
+          console.log('Registro actualizado:', result.affectedRows, 'filas afectadas');
+          res.status(200).send('Registro actualizado exitosamente');
+        }
+      }
+    );
+  });
+
+
+storageLocalizacion.delete('/lugar/del/:idLocalizacion', (req, res)=>{
+    const idLocalizacion = req.params.idLocalizacion;
+    con.query(
+        `DELETE FROM localizacion WHERE idLocalizacion = ?`,
+        idLocalizacion,
+
+        (err, result) => {
+            if (err) {
+                console.error('Error al eliminar la localidad del usuario', err.message);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    )
+});
+
+export default storageLocalizacion;
